c56b88bc1bc330e82d44a08ca3660f035345934f
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-10-23 16:10:09 +0800

    Update crud for users

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 
 function App() {
   const [languages, setLanguages] = useState([]);
+  const [users, setUsers] = useState([]);
   const languageRef = useRef();
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const countryRef = useRef();
 
   const retrieveAllLanguages = async () => {
     try {
@@ -15,7 +19,7 @@ function App() {
       const data = await res.json();
       setLanguages(data);
     } catch (error) {
-      throw new Error("error");
+      console.log(error.message);
     }
   };
 
@@ -54,45 +58,148 @@ function App() {
     retrieveAllLanguages();
   };
 
+  const retrieveAllUsers = async () => {
+    try {
+      const res = await fetch(import.meta.env.VITE_SERVER + "/lab/users");
+
+      if (!res.ok) {
+        throw new Error("error");
+      }
+
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const addUser = async () => {
+    const res = await fetch(import.meta.env.VITE_SERVER + "/lab/users", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: nameRef.current.value,
+        age: ageRef.current.value,
+        country: countryRef.current.value,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("error");
+    }
+
+    retrieveAllUsers();
+    nameRef.current.value = "";
+    ageRef.current.value = "";
+    countryRef.current.value = "";
+  };
+
+  const deleteUser = async (id) => {
+    const res = await fetch(import.meta.env.VITE_SERVER + "/lab/users", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user_id: id,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error("error");
+    }
+
+    retrieveAllUsers();
+  };
+
   useEffect(() => {
     retrieveAllLanguages();
+    retrieveAllUsers();
   }, []);
 
   return (
-    <div>
-      <h1>Language List</h1>
-      <div className="row">
-        <input
-          type="text"
-          ref={languageRef}
-          placeholder="language"
-          className="col-md-3"
-        />
-        <button className="col-md-3" onClick={addLanguage}>
+    <>
+      <h2>Language Register App</h2>
+
+      <div className="container border m-4 p-4">
+        <h4>Add a language :</h4>
+        <div className="row my-2">
+          <label className="col-sm-3">Language</label>
+          <input
+            className="col-sm-3"
+            type="text"
+            ref={languageRef}
+            placeholder="1 to 20 chars only..."
+          />
+        </div>
+        <button className="col-sm-1 mx-2" onClick={addLanguage}>
           add
         </button>
-      </div>
-      <br />
-      <br />
-      <div className={`row`}>
-        <div className="col-md-3">Language</div>
-        <div className="col-md-2"></div>
-        <div className="col-md-2"></div>
-      </div>
-      {languages.map((item, idx) => {
-        return (
-          <div key={idx} className="row">
-            <div className="col-sm-3">{item.language}</div>
+
+        <div className="row my-2"></div>
+        <div className="row my-2"></div>
+
+        <h4>All languages list :</h4>
+        {languages.map((item, idx) => (
+          <div key={idx} className="row my-2">
+            <label className="col-sm-3">{item.language}</label>
             <button
-              className="col-sm-2"
+              className="col-sm-1 mx-2"
               onClick={() => deleteLanguage(item.language)}
             >
               delete
             </button>
           </div>
-        );
-      })}
-    </div>
+        ))}
+      </div>
+
+      <div className="container border m-4 p-4">
+        <h4>Add a new user :</h4>
+        <div className="row my-2">
+          <label className="col-sm-3">Name</label>
+          <input
+            className="col-sm-3"
+            type="text"
+            ref={nameRef}
+            placeholder="1 to 50 chars only..."
+          />
+        </div>
+        <div className="row my-2">
+          <label className="col-sm-3">Age</label>
+          <input className="col-sm-3" type="number" ref={ageRef} />
+        </div>
+        <div className="row my-2">
+          <label className="col-sm-3">Country</label>
+          <input
+            className="col-sm-3"
+            type="text"
+            ref={countryRef}
+            placeholder="1 to 50 chars only..."
+          />
+        </div>
+        <button className="col-sm-1 mx-2" onClick={addUser}>
+          add
+        </button>
+
+        <div className="row my-2"></div>
+        <div className="row my-2"></div>
+
+        <h4>All users list :</h4>
+        {users.map((item) => (
+          <div key={item.id} className="row my-2">
+            <label className="col-sm-3">{item.name}</label>
+            <label className="col-sm-3">{item.age}</label>
+            <label className="col-sm-3">{item.country}</label>
+            <button
+              className="col-sm-1 mx-2"
+              onClick={() => deleteUser(item.id)}
+            >
+              delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
